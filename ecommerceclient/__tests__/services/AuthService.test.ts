02549c8c67c807598e0900fa809a54e327e4becc
IamMax279 <maksssiula@gmail.com>
2025-04-20 11:47:57 +0200

    added a test for the getJwt() method

--- ecommerceclient/__tests__/services/AuthService.test.ts
+++ ecommerceclient/__tests__/services/AuthService.test.ts
@@ -15,6 +15,44 @@ describe("AuthService", () => {
         jest.clearAllMocks()
     })
 
+    describe("getJwt", () => {
+        test("returns jwt when available in cookies", async () => {
+            (cookies.get as jest.Mock).mockReturnValue("test-token")
+
+            const result = await AuthService.getJwt()
+
+            expect(result).toBe("test-token")
+            expect(cookies.get).toHaveBeenCalledWith("jwt")
+        })
+
+        test("return jwt when only refresh token is present", async () => {
+            (cookies.get as jest.Mock).mockImplementation((key) => {
+                if (key === "jwt") return undefined
+                if(key === "refresh") return "refresh-token"
+                return undefined
+            })
+
+            jest.spyOn(AuthService, "getRefresh").mockReturnValue("refresh-token");
+
+            (axios.post as jest.Mock).mockResolvedValue({
+                data: {accessToken: 'new-access-token'}
+            })
+
+            const setJwtSpy = jest.spyOn(AuthService, 'setJwt');
+    
+            const result = await AuthService.getJwt();
+            
+            expect(result).toBe('new-access-token');
+            expect(cookies.get).toHaveBeenCalledWith('jwt');
+            expect(AuthService.getRefresh).toHaveBeenCalled();
+            expect(axios.post).toHaveBeenCalledWith(
+                '/api/user/refresh-token',
+                { refresh: 'refresh-token' }
+            );
+            expect(setJwtSpy).toHaveBeenCalledWith('new-access-token');
+        })
+    })
+
     describe("logout", () => {
         test("removes cookies and dispatches logout event", () => {
             const dispatchEventSpy = jest.spyOn(document, "dispatchEvent");
