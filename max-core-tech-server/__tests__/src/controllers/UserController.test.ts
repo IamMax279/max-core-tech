1a05b0f1821ea8340aa73ec9dad2fd7bc379ae34
IamMax279 <maksssiula@gmail.com>
2025-04-21 21:50:31 +0200

    final commit

--- max-core-tech-server/__tests__/src/controllers/UserController.test.ts
+++ max-core-tech-server/__tests__/src/controllers/UserController.test.ts
@@ -5,7 +5,7 @@ import { AuthService } from "../../../src/services/AuthService";
 
 jest.mock("../../../prisma/PrismaClient", () => ({
     __esModule: true, //bo default export
-    default: { //dlatego ze jets default export
+    default: { //dlatego ze jest default export
         user: {
             create: jest.fn()
         }
@@ -29,6 +29,43 @@ describe("UserController", () => {
             jest.clearAllMocks()
         })
 
+        test("should create a user if all the fields were provided", async () => {
+            const userData = {
+                email: "random@mail.com",
+                firstName: "John",
+                lastName: "Doe",
+                password: "Password123!"
+            };
+
+            (argon2.hash as any).mockImplementation(() => Promise.resolve("hashed-pass"));
+    
+            (prisma.user.create as any).mockImplementation(() => Promise.resolve({
+                id: 1,
+                email: userData.email,
+                firstName: userData.firstName,
+                lastName: userData.lastName,
+                password: "hashed_password",
+                createdAt: new Date()
+            }));
+            
+            (AuthService.generateEmailToken as any).mockImplementation(() => "email-token");
+            
+            (AuthService.sendVerificationEmail as any).mockImplementation(() => 
+                Promise.resolve({
+                    success: true,
+                    message: "Email sent"
+                })
+            );
+
+            const result = await UserController.registerUser(userData)
+
+            expect(result).toEqual({
+                success: true,
+                message: "User added successfully + verification email sent."
+            })
+            expect(prisma.user.create).toHaveBeenCalled()
+        })
+
         test("should return error when email is empty", async () => {
             const userData = {
                 email: "  ",
@@ -44,6 +81,43 @@ describe("UserController", () => {
                 message: "Some field(s) are empty"
             })
             expect(prisma.user.create).not.toHaveBeenCalled()
+        })
+
+        test("should return error if verification email was not sent", async () => {
+            const userData = {
+                email: "random@mail.com",
+                firstName: "John",
+                lastName: "Doe",
+                password: "Password123!"
+            };
+
+            (argon2.hash as jest.Mock).mockImplementation(() => Promise.resolve("hashed-pass"));
+
+            (prisma.user.create as jest.Mock).mockImplementation(() => Promise.resolve({
+                id: 1,
+                email: userData.email,
+                firstName: userData.firstName,
+                lastName: userData.lastName,
+                password: "hashed_pass",
+                createdAt: new Date()
+            }));
+
+            (AuthService.generateEmailToken as jest.Mock).mockImplementation(() => undefined);
+            
+            (AuthService.sendVerificationEmail as jest.Mock).mockImplementation(() => 
+                Promise.resolve({
+                    success: false,
+                    message: "email or token not provided"
+                })
+            );
+
+            const result = await UserController.registerUser(userData)
+
+            expect(AuthService.generateEmailToken).toHaveBeenCalled()
+            expect(result).toEqual({
+                success: false,
+                message: "Failed to send verification email"
+            })
         })
     })
 })
---
+++ max-core-tech-server/prisma/migrations/20250418171559_nvm2/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "user_addresses_userId_key";
